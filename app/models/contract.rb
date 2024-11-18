3a240a11922da5ca2df988fb41887ebc3c6c6078
benoit moret <benoit.moret@gmail.com>
2024-11-18 10:22:16 +0100

    repair update turbo

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,6 +1,6 @@
 class Contract < ApplicationRecord
   after_update_commit -> { broadcast_update_to "contracts", partial: "contracts/contract",
-   locals: { quote: self }, target: "contracts" }
+   locals: { quote: self }, target: "contract" }
   after_create_commit -> { broadcast_prepend_to "contracts", partial: "contracts/contract",
    locals: { quote: self }, target: "contracts" }
   after_destroy_commit -> { broadcast_remove_to "contracts", target: "contract_#{id}"}
